4955a012e38c438b17d0943c0b4385c6c5768f75
Christian Cordes <114151334+CKCordes@users.noreply.github.com>
2022-12-05 17:04:47 +0100

    solution for day 4

---
+++ 4.1/cleanup.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main() {
+    // opening data file
+    std::ifstream data;
+    data.open("data.txt");
+    int sum = 0;
+    int commaIndex = 0, dashIndex = 0;
+    std::string currLine, firstInt, secondInt;
+
+    // making sure file is open
+    if (!data.is_open()) {
+        printf("Cannot open file");
+        return -1;
+    }
+    while (true) {
+        std::getline(data, currLine);
+        if (!data) {
+            break;
+        }
+        commaIndex = currLine.find(",");
+        firstInt = currLine.substr(0, commaIndex);
+        secondInt = currLine.substr(commaIndex + 1, currLine.length());
+        // first interval, converting to int
+        dashIndex = firstInt.find("-");
+        int fLowerInt = stoi(firstInt.substr(0, dashIndex));
+        int fUpperInt = stoi(firstInt.substr(dashIndex + 1, firstInt.length()));
+
+        // second interval converting to int
+        dashIndex = secondInt.find("-");
+        int sLowerInt = stoi(secondInt.substr(0, dashIndex));
+        int sUpperInt =
+            stoi(secondInt.substr(dashIndex + 1, secondInt.length()));
+
+        std::cout << currLine << "\n" << sum << "\n\n";
+        // if the first interval contains the second:
+        if (fLowerInt <= sLowerInt && fUpperInt >= sUpperInt) {
+            sum++;
+        } else if (fLowerInt >= sLowerInt && fUpperInt <= sUpperInt) {
+            sum++;
+        }
+    }
+    std::cout << sum;
+    data.close();
+    return 0;
+}
